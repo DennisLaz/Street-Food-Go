5a70e4a6482bc6491b7a1da32ab70d45ee50d4e0
IONNYT <156337839+IONNYT@users.noreply.github.com>
2025-12-14 17:13:58 +0200

    Delete Street-Food-Go-main directory

--- Street-Food-Go-main/src/main/java/gr/hua/dit/StreetFoodGo/core/security/CurrentUser.java
+++
@@ -1,8 +0,0 @@
-package gr.hua.dit.StreetFoodGo.core.security;
-
-import gr.hua.dit.StreetFoodGo.core.model.PersonType;
-
-/**
- * @see CurrentUserProvider
- */
-public record CurrentUser (long id, String username,String emailAddress, PersonType type) {}
